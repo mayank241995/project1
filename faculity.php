25b35c1210b1d7567f0f23dbb16c8a4a2f1ce5a8
mayank <srivastava.mayank241995@gmail.com>
2018-03-07 21:38:25 +0530

    Initial commit

---
+++ faculity.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+    <title>faculity page</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="js/carousfredsel.js" type="text/javascript" ></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+   <header>
+       <div class="container">
+           <div class="row">
+               <a href=""><img src="image/uem1.png"></a>
+               <nav>
+                   <ul>
+                       
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Work</a></li>
+                        <li><a href="">Service</a></li>
+                        <li><a href="">Client</a></li>
+                        <li><a href="#Ourteam">Our Team</a></li>
+                        <li><a href="">Contact</a></li>
+                       
+                   </ul>
+               </nav>
+           </div>
+       </div>
+   </header>
+   
+   <?php 
+foreach(array as $name)
+{
+	<div class="col-sm-2 col-sm-offset-1">
+	                        <h4>Circle Image</h4>
+	                        <img src="assets/img/mayank.jpg" alt="Circle Image" class="img-circle img-responsive">
+	                    </div>
+} 
+?>
+   
+   
+   <script src="js/main.js"></script>
+    </body>
+</html>
